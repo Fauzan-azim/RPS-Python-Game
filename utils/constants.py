ca277d82e85ecb3fb0b1c720520cc27e0facee74
Fauzan Azim <92430909+Fauzan-azim@users.noreply.github.com>
2022-08-17 14:49:26 +0700

    Add files via upload

---
+++ utils/constants.py
@@ -0,0 +1,13 @@
+PAPER = 0
+ROCK = 1
+SCISSORS = 2
+
+# Define text labels corresponding to gestures
+gestureText = {ROCK: "rock", PAPER: "paper", SCISSORS: "scissors"}
+
+# Define paths to static image folders
+gestureImgPaths = {
+    PAPER: "utils/images/0.png",
+    ROCK: "utils/images/1.png",
+    SCISSORS: "utils/images/2.png",
+}
